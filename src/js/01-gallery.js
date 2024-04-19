2162ba61193412e42aca6bc7b46712275c797558
aprilespiritu <april_espiritu@live.com>
2024-04-19 15:03:19 +0800

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,13 @@
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const gallery = new SimpleLightbox(".gallery a");
+});
